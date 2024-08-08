335cbc2299887018d3020fe39e7fe37ab41810f2
kevyn levi <kevynlevi0@gmail.com>
2024-08-08 00:14:08 -0300

    novo commit

--- Projeto_DASS/app/run.py
+++ Projeto_DASS/app/run.py
@@ -3,4 +3,10 @@ app =  Flask(__name__)
 @app.route("/")
 def inicio():
     return render_template("index.html")
+
+@app.route("/quiz")
+
+def quiz():
+    return render_template("quiz.html")
+
 app.run(debug=True)
