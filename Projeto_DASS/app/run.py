d377b32021223b6b5b0be312f8b80ece288469ed
kevyn levi <kevynlevi0@gmail.com>
2024-08-11 23:37:23 -0300

    quase

--- Projeto_DASS/app/run.py
+++ Projeto_DASS/app/run.py
@@ -5,8 +5,18 @@ def inicio():
     return render_template("index.html")
 
 @app.route("/quiz")
-
 def quiz():
     return render_template("quiz.html")
-
+@app.route("/poka")
+def poka():
+    return render_template("poka.html")
+@app.route("/gestao")
+def gestao():
+    return render_template("gestao.html")
+@app.route("/kanban")
+def kanban():
+    return render_template("kanban.html")
+@app.route("/local")
+def local():
+    return render_template("local.html")
 app.run(debug=True)
