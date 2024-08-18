67caf31fe51218c4b9fdd288e02ab76dc4b871a5
BieldoPython <gabifdasilva2004@gmail.com>
2024-08-18 14:51:32 -0300

    add aba documentos

--- app/run.py
+++ app/run.py
@@ -10,19 +10,29 @@ def inicio():
 def quiz():
     flash("Boa Sorte nas perguntas", "success")
     return render_template("quiz.html")
+
 @app.route("/poka")
 def poka():
     return render_template("poka.html")
+
 @app.route("/gestao")
 def gestao():
     return render_template("gestao.html")
+
 @app.route("/kanban")
 def kanban():
     return render_template("kanban.html")
+
 @app.route("/local")
 def local():
     return render_template("local.html")
+
 @app.route("/sobre")
 def sobre():
     return render_template("sobre.html")
+
+@app.route("/documentos")
+def documentos():
+    return render_template("documentos.html")
+
 app.run(debug=True)
